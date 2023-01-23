efc18b9925b37fce7f1ad9bb7bf117b4170bd1a7
Daren Shamoun <darenshamoun@gmail.com>
2023-01-23 15:06:33 -0800

    git changes

--- build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/daren/source/repos/Big Number Postfix Calculator cpp
-# Build directory: C:/Users/daren/source/repos/Big Number Postfix Calculator cpp/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- build/x64-Debug/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/daren/source/repos/Big Number Postfix Calculator cpp/src
-# Build directory: C:/Users/daren/source/repos/Big Number Postfix Calculator cpp/build/x64-Debug/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
